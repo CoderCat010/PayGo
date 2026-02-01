2ffe7c6038d051590105a655c05fd9b49d790422
CoderCat010 <nusraumme@gmail.com>
2026-02-01 14:16:21 +0600

    added dashboard

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 document.getElementById('login-btn').addEventListener('click', function(event){
    event.preventDefault();
-   const account = document.getElementById('emailAccount').value;
-   console.log(account);
 
+   // get account number
+   const account = document.getElementById('accountNumber').value;
+
+   // get password 
    const password = document.getElementById('passwordInfo').value;
-   console.log(password);
+
+   // check if pin and account number is valid 
+   if(account.length === 11){
+      if(password.length === 4){
+         window.location.href = './dashboard.html'
+      }else{
+         alert('Please provide valid pin number') 
+      }
+   }else{
+      alert('Please provide valid account number')
+   }
+
 })
