af4d92f80793f2f8d299b501cd96fc1755b9a86b
CoderCat010 <nusraumme@gmail.com>
2026-03-01 11:06:03 +0600

    added add money and cashot section functionality

---
+++ sections-toggle.js
@@ -0,0 +1,4 @@
+document.getElementById('add-money-card').addEventListener('click', function(){
+    document.getElementById('cashout-section').style.display = 'none';
+    document.getElementById('add-money-section').style.display = 'block';
+})
