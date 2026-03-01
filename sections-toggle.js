3cef5fdb62ca1b326e615c0400724f7ca30df1a8
CoderCat010 <nusraumme@gmail.com>
2026-03-01 11:18:28 +0600

    added add money all functionality

--- sections-toggle.js
+++ sections-toggle.js
@@ -1,4 +1,21 @@
+// add money section
 document.getElementById('add-money-card').addEventListener('click', function(){
+    // hide cashout section by add money card
     document.getElementById('cashout-section').style.display = 'none';
+
+    // hide transfer money section by add money card
+    document.getElementById('transfer-money-section').style.display = 'none';
+
+    // hide get bonus section by add money card
+    document.getElementById('get-bonus-section').style.display = 'none';
+
+    // hide pay bill section by add money card
+    document.getElementById('pay-bill-section').style.display = 'none';
+
+    // add money card
     document.getElementById('add-money-section').style.display = 'block';
 })
+
+
+
+
