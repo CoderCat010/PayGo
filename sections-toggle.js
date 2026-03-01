017ad441f1bd30b7407b67202e0be8c370868295
CoderCat010 <nusraumme@gmail.com>
2026-03-01 11:24:05 +0600

    added cashout all functionality

--- sections-toggle.js
+++ sections-toggle.js
@@ -1,4 +1,4 @@
-// add money section
+// add money button & add money section 
 document.getElementById('add-money-card').addEventListener('click', function(){
     // hide cashout section by add money card
     document.getElementById('cashout-section').style.display = 'none';
@@ -14,6 +14,26 @@ document.getElementById('add-money-card').addEventListener('click', function(){
 
     // add money card
     document.getElementById('add-money-section').style.display = 'block';
+})
+
+
+
+// cashout button & cashout section
+document.getElementById('cashout-card').addEventListener('click', function(){
+    // hide add money section by cashout card
+    document.getElementById('add-money-section').style.display = 'none';
+
+    // hide transfer money section by cashout card
+    document.getElementById('transfer-money-section').style.display = 'none';
+
+    // hide get bonus section by cashout card
+    document.getElementById('get-bonus-section').style.display = 'none';
+
+    // hide pay bill section by cashout card
+    document.getElementById('pay-bill-section').style.display = 'none';
+
+    // add money card
+    document.getElementById('cashout-section').style.display = 'block';
 })
 
 
