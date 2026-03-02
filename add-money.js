b1064e49e5f6d07494c5b766d72e15a0739e4a2a
CoderCat010 <nusraumme@gmail.com>
2026-03-02 08:05:33 +0600

    added few functionality of add money section

--- add-money.js
+++ add-money.js
@@ -23,8 +23,29 @@ document.getElementById('add-money').addEventListener('click', function(event){
 
     // sum amount and current balance and update current balance
     if(pin.length === 4){
+        // add total balance to main balance
         const balanceSum = convertedAmount + convertedBalance;
         document.getElementById('current-balance').textContent = balanceSum;
+
+        // add -----> add money history to transaction history
+        const history = document.createElement('div');
+
+        // child 1
+        const div1 = document.createElement('div');
+        const div1Img = document.createElement('img');
+        div1Img.src = './images/money1.png';
+        div1.append(div1Img);
+
+        // // child 2
+        // const div2 = document.createElement('div')
+
+
+
+
+        
+        // // child 3
+        // const div3 = document.createElement('div')
+
     }else{
         alert('pin must be 4 digits')
     }
